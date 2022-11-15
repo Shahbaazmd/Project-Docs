beb998e3f8a4ee88d9bd336c2366620052bd7570
Shahbaazmd <117228493+Shahbaazmd@users.noreply.github.com>
2022-11-15 17:06:17 -0600

    Add files via upload

---
+++ dataload.sql
@@ -0,0 +1,31 @@
+use ecomdb;
+LOAD DATA INFILE 
+ 'C:\Users\13124\OneDrive\Desktop\Admin_details.csv' 
+into table ecomdb.user1 
+fields terminated by ',';
+
+use ecomdb;
+LOAD DATA INFILE 
+ 'C:\Users\13124\OneDrive\Desktop\Admin_details.csv' 
+into table ecomdb.user1 
+fields terminated by ',';
+
+use ecomdb;
+LOAD DATA INFILE 
+ 'C:\Users\13124\OneDrive\Desktop\Admin_details.csv' 
+into table ecomdb.user1 
+fields terminated by ',';
+
+use ecomdb;
+LOAD DATA INFILE 
+ 'C:\Users\13124\OneDrive\Desktop\Admin_details.csv' 
+into table ecomdb.user1 
+fields terminated by ',';
+
+use ecomdb;
+LOAD DATA INFILE 
+ 'C:\Users\13124\OneDrive\Desktop\Admin_details.csv' 
+into table ecomdb.user1 
+fields terminated by ',';
+
+
